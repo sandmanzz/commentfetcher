8b57d8cb3d281fa7a04346e91b9ce8eebba36c18
Chandra <chandra@Chandras-MacBook-Pro.local>
2025-05-28 23:46:04 +0700

    45th push

--- pages/api/comments.js
+++ pages/api/comments.js
@@ -1,5 +1,18 @@
 
 export default async function handler(req, res) {
+     if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', '*'); // or 'null' for Figma plugin
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.status(200).end(); // important: must return 200 OK
+    return;
+  }
+
+
+  // Actual request (GET, POST, etc.)
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   const { token, fileKey } = req.query;
 
   if (!token || !fileKey) {
